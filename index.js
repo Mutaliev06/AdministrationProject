8afaabfeece1fcd26f88e6162e98320c5fd81ff1
mutaliev06 <maga_mutaliev06@mail.ru>
2021-07-29 10:37:07 +0300

    add Heroku

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const morgan = require("morgan");
 
 const app = express();
 const { port, url } = require("./config/index");
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use(express.static(path.resolve(__dirname, "client", "build")));
+
 app.use(router);
+
+app.get('*', (req, res) => {
+  res.send(path.resolve(__dirname, "client", "build", "index.html"))
+})
 
 async function start() {
   try {
